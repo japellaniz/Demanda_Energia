d50ff177480ac00129d84bbe8013c65309de0c53
japellaniz <jose.apellaniz@gmail.com>
2020-12-07 14:13:49 +0100

    7/12/2020

--- energy_weather.R
+++ energy_weather.R
@@ -1,5 +1,8 @@
 
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Inicialización y carga de librerías ####
 rm(list = ls())
+graphics.off()
 
 # Cambiar el directorio de trabajo
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
@@ -15,8 +18,11 @@ library(funModeling)
 library(corrplot)
 library(lubridate)
 library(Metrics)
+library(caret)
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Carga de datos ####
 df1 <- read_csv("data/energy_dataset.csv")
 df2 <- read_csv("data/weather_features.csv")
 
@@ -25,8 +31,12 @@ str(df2)
 
 df_status(df1)
 df_status(df2)
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-# Arreglamos datos en df1++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Limpieza y calidad de datos ####
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Arreglamos datos en df1
 # Eliminamos columnas vacias (100% NAs)
 df1[,c(11,24)] <- NULL
 # Eliminamos columnas con 99,95% nulos.
@@ -34,6 +44,8 @@ df1[,c(4,8,9,10,14,20)] <- NULL
 # Eliminamos filas con mayoria NAs pero primero tomamos nota de las fechas/horas eliminadas. 
 df_dates_na <- df1 %>% filter(is.na(df1[,c(2,3,4,5,6,7,8,9,10,11,12,13,14,15)])) %>% .$time
 df1 <- df1 %>%  filter(!is.na(df1[,c(2,3,4,5,6,7,8,9,10,11,12,13,14,15)]))
+
+
 # Asignación de la media a valores sueltos de NAs
 df1[6][is.na(df1[6])] =  colMeans(df1[6], na.rm = TRUE)
 df1[7][is.na(df1[7])] =  colMeans(df1[7], na.rm = TRUE)
@@ -42,7 +54,7 @@ df1[14][is.na(df1[14])] =  colMeans(df1[14], na.rm = TRUE)
 df1[19][is.na(df1[19])] =  colMeans(df1[19], na.rm = TRUE)
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-# Arreglamos datos en df2 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Arreglamos datos en df2 
 # Adaptamos tipos de datos
 df2[, c(2,15,16,17)] <- lapply(df2[, c(2,15,16,17)],as.factor)
 levels(df2$city_name)
@@ -97,7 +109,9 @@ df2_m_pres <- df2 %>% select(6) %>% reshape2::melt()
 plotar(df2_m_pres,  target= "variable", input = "value", plot_type = "boxplot")
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-# EDA +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Análisis Exploratorio de Datos (EDA) ####
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Vemos la distribución de los datos por columnas
 plot_num(df1[,-1], bins = 20)
 plot_num(df2[,-1], bins = 20)
@@ -106,7 +120,7 @@ plot_num(df2[,-1], bins = 20)
 # # Datos solo del 2018.
 # df11 <- df1[year(df1$time)== 2018,]
 
-# Comparativa de demanda energética
+# Comparativa de demanda energética +++++++++++++++++++++++++++++++++++++++++++++++++
 df1 %>% ggplot(aes(x=time)) +
   #geom_line(aes(y= df1$`total load actual`), alpha = 0.3, col = "orangered2") +
   geom_smooth(aes(y= df1$`total load actual`, col = "Demanda actual"))+
@@ -166,7 +180,7 @@ df12g %>% ggplot(aes(x=dia, y=value, fill =variable, col =variable)) +
 # nos vamos a centrar en la predicción del precio.
 
  
-# Comparativa de precios actual y predicho.
+# Comparativa de precios actual y predicho.+++++++++++++++++++++++++++++++++++++++++++
 df1 %>% ggplot(aes(x=time)) +
   geom_line(aes(y= df1$`price actual`), alpha = 0.3, col = "orangered2") +
   geom_smooth(aes(y= df1$`price actual`, col = "Precio actual"))+
@@ -196,6 +210,8 @@ df1 %>% mutate(dif = df1$`price actual`-df1$`price day ahead`) %>%
   geom_density(aes(dif), col = "steelblue", size = 1)+
   labs(x = "Diferencia precio actual-predicción", title = "Diferencia precio actual vs predicción TSO") +
   theme_bw()
+# Donde apreciamos una clara desviación respecto al cero.
+
 
 # Modificamos la tabla de clima obteniendo las medias de las cinco ciudades 
 # con los valores numéricos para cada hora.
@@ -216,21 +232,24 @@ df21 <- df2 %>% select(1,3:13) %>%
 
 # Ahora podemos juntar las 2 tablas
 # df21 <- df21[year(df21$time)== 2018,]
-df <- df1 %>% inner_join(df21,by="time")
+data <- df1 %>% inner_join(df21,by="time")
 
 # Variables más correlacionadas con las variables objetivo
-correlation_table(df, "total load actual")
-correlation_table(df, "price actual")
+correlation_table(data, "total load actual")
+correlation_table(data, "price actual")
 
 
-summary(df)
-str(df)
+summary(data)
+str(data)
 
-# Importancia de variables con Random Forest
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Preparación para el modelo predicitivo ####
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Importancia de variables con Random Forest++++++++++++++++++++++++++++++++++++++++++++++++
 # Ejecutamos Random Forest para ver cuáles son las variables explicativas más importantes.
 library(randomForest)
 set.seed(1234)
-train_forest <- df %>%
+train_forest <- data %>%
   select(-time) %>% 
   filter(!is.na(`price actual`))
 
@@ -250,14 +269,14 @@ ggplot(random_forest[1:30,], aes(x=reorder(Variables, -MSE), y=MSE, group=1)) +
   labs(x = "Variable", y= "MSE") + 
   labs(title = "MSE explanation evolution") + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
-
-# Análisis de multicolinearidad.
+# Análisis de multicolinearidad.++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Cálculo de la correlación y filtro para aquellas que sean mayor que |0.5| 
 # y toma de los nombres de dichas variables
 library(psych)
-df_cor <- df[,-1]
+df_cor <- data[,-1]
 correlations <- as.matrix(x = sort(cor(df_cor, use="pairwise.complete.obs")[,"price actual"], decreasing = TRUE))
 # Hacemos un pequeño filtro
 names <- names(which(apply(correlations,1, function(x) abs(x)>0.1))) 
@@ -268,9 +287,72 @@ df_cor <- df_cor[, names]
 # Crear y representar la matriz de correlación
 correlations <- cor(df_cor, use="pairwise.complete.obs")
 cor.plot(correlations, numbers=TRUE, xlas = 2, upper= FALSE, main="Correlaciones entre las variables", zlim=c(abs(0.65),abs(1)), colors=TRUE)
-# Vemos qué variables están más correlacionadas para descartar redundancias
+# Vemos qué variables están más correlacionadas para descartar redundancias:
 # "generation biomass" con "generation other" (0,66). Cogemos "generation biomass"
 # "generation fossil hard coal" con "price day ahead" (0,67) y con "generation fossil brown coal/lignite" (0,77).
-# Cogemos "generation fossil hard coal"
+# Cogemos "price day ahead"
 # "total load actual" con "total load forecast" (0,99). Cogemos "total load actual".
 # "generation wind onshore" con "generation wind onshore day ahead" (0,99). Cogemos "generation wind onshore".
+
+df <- data[,-c(3,5,11,17,18)]
+rmse(df$`price actual`,df$`price day ahead`)
+
+# Pendiente:
+# Las horas hay que ponerlas como dummy var ya que están como tipo date, no numérico.
+# Hay que hacer partición train-test (80-20) con la variable "price actual" como
+# target. Comprobar resultados con el dataset de test.
+
+df_train <- df %>% mutate(hora = hour(time)) %>% 
+  fastDummies::dummy_cols(select_columns = "hora") %>%
+  select(-c("time", "hora"))
+
+# Separamos en train y test
+train <- createDataPartition(y = df_train$`price actual`, p = 0.8, list = FALSE, times = 1)
+datos_train <- df_train[train, ]
+datos_test  <- df_train[-train, ]
+# Comprobamos la semejante distribución de la variable objetivo en los dataset de train y test.
+ggplot()+
+  geom_density(data = datos_train, aes(x=`price actual`, fill = "Train"), alpha =0.5)+
+  geom_density(data = datos_test, aes(x = `price actual`, fill = "Test"), alpha = 0.5)+
+  scale_fill_manual("", guide = "legend",
+                    values = c("Train" = "orangered2",
+                               "Test" = "gray50"))+
+  theme_bw() +
+  theme(legend.position = "bottom")
+
+# Primera prueba de entrenamiento de un modelo de regresión lineal
+modelo_glm <- train(`price actual` ~ ., method = "glm", data = datos_train)
+modelo_glm$resample %>% head(10)
+
+# Utilizando resampling
+particiones  <- 10
+repeticiones <- 5
+
+control_train <- trainControl(method = "repeatedcv", number = particiones,
+                              repeats = repeticiones,
+                              returnResamp = "all", verboseIter = FALSE,
+                              allowParallel = TRUE)
+
+# Ajuste del modelo
+modelo_lm <- train(`price actual` ~ ., data = datos_train,
+                          method = "lm",
+                          metric = "RMSE",
+                          trControl = control_train)
+modelo_lm
+modelo_lm$resample %>% head(10)
+summary(modelo_lm$resample$RMSE)
+
+
+predicciones_lm <- predict(modelo_lm, newdata = datos_test,
+                            type = "raw")
+
+RMSE(datos_test$`price actual`, predicciones_lm)
+ggplot() +
+  geom_density(aes(datos_test$`price actual`, fill = "Precio actual") ,alpha = 0.3) +
+  geom_density(aes(predicciones_lm, fill = "Precio predicción"),alpha = 0.3)+
+  scale_fill_manual("", guide = "legend",
+                    values = c("Precio actual" = "orangered2",
+                               "Precio predicción" = "gray50"))+
+  labs(x = "Precio", title = "Precio actual vs predicción") +
+  theme_bw() +
+  theme(legend.position = "bottom")
